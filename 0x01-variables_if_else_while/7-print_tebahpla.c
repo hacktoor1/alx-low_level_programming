6302d261aa4ab72574df47a3511e88959d302dbf
hacktoor1 <98988408+hacktoor1@users.noreply.github.com>
2023-03-18 02:16:02 +0200

    hello

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the
+ *
+ * Return: Always 0 (Sucess)
+ **/
+int main(void)
+{
+        char alf[26] = "zyxwvutsrqponmlkjihgfedcba";
+        int i;
+
+        for (i = 0 ; i < 26 ; i++)
+        {
+                putchar(alf[i]);
+        }
+        putchar('\n');
+        return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - prints rhw alf whitout e and q
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+        char alf[26] = "123456789abcdef";
+        int i;
+
+        for (i = 0 ; i < 26 ; i++)
+        {
+                if (i != 4 && i != 17)
+                {
+                        putchar(alf[i]);
+                }
+        }
+        putchar('\n');
+        return (0);
+}
