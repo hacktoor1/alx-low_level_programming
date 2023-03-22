5a0f9accb5ceac6d4ae3ba5cee51fb4240342428
hacktoor1 <98988408+hacktoor1@users.noreply.github.com>
2023-03-22 02:16:35 +0200

    hello

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Enter point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
