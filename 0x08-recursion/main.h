05d36881fbfb3091a6cd1e73980d7ad8f4cd3701
hacktoor1 <98988408+hacktoor1@users.noreply.github.com>
2023-04-07 04:32:01 +0200

    hello

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -8,3 +12,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
+#endif
